ef3b6e5a8c19d0d183c786f376b0043ba9e8ac93
unsane <109696941+ingmarAvocado@users.noreply.github.com>
2025-08-31 22:05:33 -0600

    feat(bots): add Bots WebSocket handler, router, real Redis tests and example (#8) (#29)

--- src/fullon_cache_api/main.py
+++ src/fullon_cache_api/main.py
@@ -12,6 +12,7 @@ from fullon_log import get_component_logger  # type: ignore
 
 
 from .routers.accounts import router as accounts_router
+from .routers.bots import router as bots_router
 from .routers.orders import router as orders_router
 from .routers.tickers import router as tickers_router
 from .routers.websocket import router as ws_router
@@ -23,6 +24,7 @@ def create_app() -> FastAPI:
     app = FastAPI(title="fullon_cache_api", docs_url=None, redoc_url=None)
     app.include_router(ws_router)
     app.include_router(tickers_router)
+    app.include_router(bots_router)
     app.include_router(orders_router)
     app.include_router(accounts_router)
     logger.info("FastAPI WebSocket app created")
